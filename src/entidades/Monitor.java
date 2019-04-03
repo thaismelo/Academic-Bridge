a68aec6a6484aa3e2eed79fb4432eb46d0075fda
Thais Melo <thaiscapoeiras1999@hotmail.com>
2019-04-03 01:07:12 -0300

    adicionando monitor

---
+++ src/entidades/Monitor.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author thais
+ */
+public class Monitor extends Pessoa{
+    private String codigo;
+    private String senha;
+
+    public Monitor() {
+    }
+
+    public Monitor(String codigo, String senha, String nome, String email) {
+        super(nome, email);
+        this.codigo = codigo;
+        this.senha = senha;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor" + this.getNome()+"{" + "codigo=" + this.getCodigo() + ", senha=" + this.getSenha() + '}';
+    }
+    
+    
+    
+}
