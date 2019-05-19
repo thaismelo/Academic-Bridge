aeb31714c538b79e8268bd49790ffa020dd9cfac
Guilherme Silva <guilherme.dna06@gmail.com>
2019-05-19 01:01:13 -0300

    Adicionado repostorio Tarefa.

---
+++ src/dados/implementacoes/RepositorioTarefa.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dados.implementacoes;
+
+import dados.DAO_SQLite;
+import dados.ExceptionErroNoBanco;
+import dados.RepositorioGenerico;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import negocio.modelo.Tarefa;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class RepositorioTarefa  implements RepositorioGenerico<Tarefa>{
+
+    @Override
+    public void inserir(Tarefa tarefa) throws ExceptionErroNoBanco {
+        try {
+            Connection conn = DAO_SQLite.getSingleton().getConnection();
+            String sql = "INSERT INTO Tarefa (conteudo,estado) VALUES(?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, tarefa.getConteudo());
+            pstmt.setInt(2, tarefa.getEstado());
+            pstmt.executeUpdate();
+            ResultSet resultSet = null;
+            PreparedStatement preparedStatement = null;
+            sql = "SELECT * FROM Tarefa WHERE id = (select MAX(ID) from Tarefa);";
+            preparedStatement = conn.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                tarefa.setId(resultSet.getInt("id"));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException ex) {
+            throw new ExceptionErroNoBanco(ex.getMessage());
+        }    
+    }
+
+    @Override
+    public void excluir(Tarefa tarefa) throws ExceptionErroNoBanco {
+        try {
+            Connection conn = DAO_SQLite.getSingleton().getConnection();
+            ResultSet rs = null;
+            String sql = "DELETE FROM Tarefa WHERE id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, tarefa.getId());
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException ex) {
+            throw new ExceptionErroNoBanco(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void alterar(Tarefa tarefa) throws ExceptionErroNoBanco {
+        try {
+            Connection conn = DAO_SQLite.getSingleton().getConnection();
+            String sql = "UPDATE Tarefa SET conteudo = ?, estado = ? WHERE id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, tarefa.getConteudo());
+            pstmt.setInt(2, tarefa.getEstado());
+            pstmt.setInt(3, tarefa.getId());
+            pstmt.executeUpdate();
+            pstmt.close();
+
+        } catch (SQLException ex) {
+            throw new ExceptionErroNoBanco(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Tarefa recuperar(int codigo) throws ExceptionErroNoBanco {
+        try {
+            ResultSet resultSet = null;
+            Connection conn = DAO_SQLite.getSingleton().getConnection();
+            String sql = "SELECT * FROM Tarefa WHERE id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, codigo);
+            resultSet = pstmt.executeQuery();
+            while (resultSet.next()) {
+                return new Tarefa(resultSet.getInt("id"), resultSet.getString("conteudo"), resultSet.getInt("estado"));
+            }
+            resultSet.close();
+            pstmt.close();
+
+        } catch (SQLException ex) {
+            throw new ExceptionErroNoBanco(ex.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Tarefa> recuperarTodos() throws ExceptionErroNoBanco {
+        try {
+            ResultSet resultSet = null;
+            Connection conn = DAO_SQLite.getSingleton().getConnection();
+            String sql = "SELECT * FROM Tarefa;";
+            Statement stmt = conn.createStatement();
+            resultSet = stmt.executeQuery(sql);
+            List<Tarefa> listaTarefa= new ArrayList<>();
+            while (resultSet.next()) {
+                listaTarefa.add(new Tarefa(resultSet.getInt("id"), resultSet.getString("conteudo"), resultSet.getInt("estado")));
+            }
+            resultSet.close();
+            stmt.close();
+            return listaTarefa;
+        } catch (SQLException ex) {
+            throw new ExceptionErroNoBanco(ex.getMessage());
+        }
+    }
+    
+}
